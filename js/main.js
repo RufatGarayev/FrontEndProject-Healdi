b3c35ee161579002870fdca0581538f07caf9adc
Your Name <you@example.com>
2020-09-27 01:31:48 +0400

    banner-section style has been changed and services section temporarily finished

--- js/main.js
+++ js/main.js
@@ -1,6 +1,6 @@
 let navLinks = document.querySelectorAll("nav .menu li a");
 
-// nav-links hover effects
+// nav-links hover effects start
 navLinks.forEach((link) => {
   link.addEventListener("mouseenter", function () {
     link.style.transition = "0.2s linear";
@@ -15,7 +15,9 @@ navLinks.forEach((link) => {
     link.lastChild.classList.remove("fa-angle-up");
   });
 });
+// nav-links hover effects end
 
+// play-box popup effect start
 $(document).ready(function () {
   $(".play-btn").click(function () {
     $(".play-box").css("display", "flex");
@@ -27,3 +29,24 @@ $(document).ready(function () {
     }
   });
 });
+// play-box popup effect end
+
+// services-owlCarousel start
+$('.owl-carousel').owlCarousel({
+  loop:false,
+  margin:30,
+  nav:false,
+  dot: true, 
+  responsive:{
+      0:{
+          items:1
+      },
+      600:{
+          items:2
+      },
+      1000:{
+          items:3
+      }
+  }
+})
+// services-owlCarousel end
