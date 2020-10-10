3bfafd709edcc39d81dabf9effde5c498a7f2ca3
Your Name <you@example.com>
2020-10-11 02:23:42 +0400

    some sections of about-us page were added

---
+++ js/aboutUs.js
@@ -0,0 +1,19 @@
+// about-us services-owlCarousel start
+$(".owl-carousel.myservices-2").owlCarousel({
+    loop: false,
+    margin: 30,
+    nav: false,
+    dot: true,
+    responsive: {
+      0: {
+        items: 1
+      },
+      600: {
+        items: 1
+      },
+      1000: {
+        items: 3
+      }
+    }
+  });
+  // about-us services-owlCarousel end
