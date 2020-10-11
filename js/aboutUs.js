8d549ff7aa3744bd12ccc63003acb494b1c57c46
Your Name <you@example.com>
2020-10-11 20:36:19 +0400

    about-us section finished except header

--- js/aboutUs.js
+++ js/aboutUs.js
@@ -17,3 +17,22 @@ $(".owl-carousel.myservices-2").owlCarousel({
     }
   });
   // about-us services-owlCarousel end
+
+  // testomonials-owlCarousel start
+$(".owl-carousel.myowl2").owlCarousel({
+  loop: true,
+  margin: 30,
+  dotsEach: 2,
+  responsive: {
+    0: {
+      items: 1
+    },
+    600: {
+      items: 1
+    },
+    1000: {
+      items: 2.6
+    }
+  }
+});
+// testomonials-owlCarousel end
